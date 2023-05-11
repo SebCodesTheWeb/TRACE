556f995ecd5c7ce973d06ca19e8721fa1f5db65b
Sebastian Delgado <sebastian.delgado@mediatool.com>
2023-05-11 13:43:57 +0200

    fix(client): adjusted initial camera position

--- src/client/client.ts
+++ src/client/client.ts
@@ -64,7 +64,7 @@ const raycaster = new THREE.Raycaster()
 const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.set(50, 50, 50)
+camera.position.set(25, 100, 25)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
